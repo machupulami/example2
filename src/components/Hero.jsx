41fe5d8184f95ba18c42e801e372614a4e98ed03
Anitha <anitha42747@gmail.com>
2024-01-09 21:48:04 +0530

    hero created

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>hi toni how are you
+        <h2>aikhjhdfbjdsbjbfjvdsjdbsjsddkjmknhjbdjusgbsjbhdsvfhvfhvdhvhdsvkhsvaval</h2>
+    </div>
+  )
+}
+
+export default Hero

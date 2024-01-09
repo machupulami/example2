50f8cccd5c3475aec3aa0f49a7edbd9a7c32e849
Anitha <anitha42747@gmail.com>
2024-01-09 21:49:13 +0530

    third commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,6 +4,9 @@ const Hero = () => {
   return (
     <div>hi toni how are you
         <h2>aikhjhdfbjdsbjbfjvdsjdbsjsddkjmknhjbdjusgbsjbhdsvfhvfhvdhvhdsvkhsvaval</h2>
+        <h3>hbujbjsdnhjhjs</h3>
+        <h4>bjbjcbjsbjbdsjbjsbsd</h4>
+        <h1>njndsknkndsl</h1>
     </div>
   )
 }
